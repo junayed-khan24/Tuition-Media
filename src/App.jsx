e88cc77baec1992cf29432e477c8894fc9e01d34
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-06-17 12:47:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+   <div className="App">
+      <h1 className="text-7xl font-bold text-center text-red-900">Welcome to Tuition Media</h1>
+   </div>
+  )
+}
+
+export default App
